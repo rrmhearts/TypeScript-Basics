4673185ccd3aa50714d3a59079b8587d71fbb8af
Ryan <rrmhearts@gmail.com>
2020-04-06 11:12:53 -0400

    strict checking errors.

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 "use strict";
-class Greeter {
-    constructor(n) {
+var Greeter = /** @class */ (function () {
+    function Greeter(n) {
         this.name = n;
     }
-    sayHello() {
+    Greeter.prototype.sayHello = function () {
         console.log("Hello " + this.name);
-    }
-}
+    };
+    return Greeter;
+}());
 function sortByName(a) {
     var result = a.slice(0);
-    result.sort((x, y) => {
+    result.sort(function (x, y) {
         return x.name.localeCompare(y.name);
     });
     return result;
--- main.ts
+++ main.ts
@@ -25,3 +25,17 @@ sortByName([
     {name: "Ryan", age: 10},
     {name: "Amy", age: 9}
 ]);
+
+function countLines (text?: string[]): number {
+    let count: number;
+    for (const line of text) {
+        if (line.length !== 0) {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+let a = countLines(["one", "two", "", "three"]);
+let b = countLines(["hello", null, "world"]);
+let c = countLines();
